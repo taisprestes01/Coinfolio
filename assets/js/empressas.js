50792eb97e5c86297906dd4d0c0f19f4c87d97b5
juanp89 <166066656+juanp89@users.noreply.github.com>
2024-04-13 19:36:53 -0300

    teste2

--- assets/js/empressas.js
+++ assets/js/empressas.js
@@ -1,23 +1,4 @@
-async function fetchData() {
-    try {
-      const response = await fetch('https://api.coingecko.com/api/v3/companies/public_treasury/bitcoin');
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.error('Erro ao buscar dados da API:', error);
-      return null;
-    }
-  }
-  
-  async function processData() {
-    const data = await fetchData();
-    if (data) {
-      // Aqui pode manipular os dados conforme necessário
-      console.log(data);
-    } else {
-      console.error('Nenhum dado retornado da API.');
-    }
-  }
-  
-  processData();
-  
+fetch("https://api.coingecko.com/api/v3/companies/public_treasury/bitcoin")
+.then(resposta => resposta.json())
+.then(respota => console.log(respota))
+.catch(err => console.log(err));
