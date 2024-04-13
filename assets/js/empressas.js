f44485f57d565eb8eeba94ac20938b5f5d06249b
juanp89 <166066656+juanp89@users.noreply.github.com>
2024-04-13 20:50:22 -0300

    remove page

--- assets/js/empressas.js
+++
@@ -1,4 +0,0 @@
-fetch("https://api.coingecko.com/api/v3/companies/public_treasury/bitcoin")
-.then(resposta => resposta.json())
-.then(respota => console.log(respota))
-.catch(err => console.log(err)); aaaaaa
